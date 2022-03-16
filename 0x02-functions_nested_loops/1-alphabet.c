bc1108832527a1c9e27baf4669b1c9997df9cff4
davidkah <tchek.kah@uvci.edu.ci>
2022-03-16 11:08:24 -0700

    programme alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_alphabet(void){
+
+    char c;
+    for (c='a'; c<='z' ; c++) 
+    printf ( " %c  \n", c);
+    
+}
+
+
+int main(void)
+{
+   print_alphabet() ;
+   return (0);
+}
