01fd11f5dc2b88886ff53abb6ba00afd0cf418b0
davidkah <tchek.kah@uvci.edu.ci>
2022-03-16 13:40:33 -0700

    programme sur les foctions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,10 @@
-
 #include "main.h"
 
 void print_alphabet(void){
 
     char c;
     for (c='a'; c<='z' ; c++) 
-      _putchar( " %c  \n", c);
+    printf ( " %c  \n", c);
+    _putchar('\n');
     
-}
-
-
-int main(void)
-{
-   print_alphabet() ;
-   return (0);
 }
