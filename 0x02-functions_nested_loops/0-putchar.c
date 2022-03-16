b3e21916afb4ccceb49d4e74fa520ad2452fa75a
davidkah <tchek.kah@uvci.edu.ci>
2022-03-16 13:31:51 -0700

    programme alphabet

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+main - entry block
+**/
+
+int main(void)
+{
+    char ermias[] = "_putchar";
+    int c;
+    for (c = 0; c< 8; c++ )
+    {
+        _putchar(ermias[c]);
+    }
+    -putchar('\n');
+    return (0);
+}
